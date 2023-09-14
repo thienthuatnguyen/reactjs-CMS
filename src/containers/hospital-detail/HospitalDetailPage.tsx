5ec7f06b25048aefd2bb54884c037a41d1f2a964
thuatnguyen <thuat.nguyen@opswat.com>
2023-09-14 11:36:24 +0700

    HTML detail

--- src/containers/hospital-detail/HospitalDetailPage.tsx
+++ src/containers/hospital-detail/HospitalDetailPage.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./HospitalDetailPage.scss";
-import { Tab, Tabs } from "@material-ui/core";
+import { Button, Tab, Tabs } from "@material-ui/core";
 // import { useNavigate } from "react-router-dom";
+import imgHospital from "../../assets/images/khoa.png";
+import imgHospitalDetail from "../../assets/images/bv-detail.png";
+
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
@@ -42,38 +45,186 @@ function HospitalDetailPage() {
     setValue(newValue);
   };
 
+  const GeneralInfo = () => (
+    <div className="general-info">
+      <div className="image-hospital">
+        <img src={imgHospital} alt="khoa" />
+      </div>
+      <div className="info">
+        <div className="top">
+          <div className="name">Bệnh viện chợ rẫy</div>
+          <div className="address">436/4 Nguyễn Thị Minh Khai, quận 10, thành phố Hồ Chí Minh</div>
+        </div>
+        <div className="bottom">
+          <Button
+            variant="contained"
+            className="my-btn btn-blue btn-contained">
+            Lễ tân bệnh viện
+          </Button>
+          <Button
+            variant="contained"
+            className="my-btn btn-blue btn-contained">
+            Đặt lịch hẹn
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+
+  const DetailInfo = () => (
+    <div className="detail-info">
+      <img src={imgHospitalDetail} alt="khoa" />
+      <p>
+        When the modal dialog is displayed on the screen, the keyboard focus moves to the end of the modal dialog content instead of moving at the beginning of the modal dialog content.
+        When the modal dialog is displayed on the screen, the keyboard focus moves to the end of the modal dialog content instead of moving at the beginning of the modal dialog content.
+        When the modal dialog is displayed on the screen, the keyboard focus moves to the end of the modal dialog content instead of moving at the beginning of the modal dialog content.
+        When the modal dialog is displayed on the screen, the keyboard focus moves to the end of the modal dialog content instead of moving at the beginning of the modal dialog content.
+        When the modal dialog is displayed on the screen, the keyboard focus moves to the end of the modal dialog content instead of moving at the beginning of the modal dialog content.
+        When the modal dialog is displayed on the screen, the keyboard focus moves to the end of the modal dialog content instead of moving at the beginning of the modal dialog content.
+        When the modal dialog is displayed on the screen, the keyboard focus moves to the end of the modal dialog content instead of moving at the beginning of the modal dialog content.
+      </p>
+    </div>
+  )
+
+  const TimeWork = () => (
+    <div className="time-work">
+      <h2 className="title">Thời gian làm việc</h2>
+      <div className="table-schedule">
+        <div className="row-item header">
+          <div className="col-item col-date">
+            Ngày
+          </div>
+          <div className="col-item col-time">
+            Thời gian
+          </div>
+          <div className="col-item col-action">
+
+          </div>
+        </div>
+        <div className="row-item">
+          <div className="col-item col-date">
+            Thứ 2
+          </div>
+          <div className="col-item col-time">
+            17:00 - 19:00
+          </div>
+          <div className="col-item col-action">
+            <Button
+              variant="outlined"
+              className="my-btn btn-blue btn-outlined">
+              Đặt lịch hẹn
+            </Button>
+          </div>
+        </div>
+
+        <div className="row-item">
+          <div className="col-item col-date">
+            Thứ 3
+          </div>
+          <div className="col-item col-time">
+            17:00 - 19:00
+          </div>
+          <div className="col-item col-action">
+            <Button
+              variant="outlined"
+              className="my-btn btn-blue btn-outlined">
+              Đặt lịch hẹn
+            </Button>
+          </div>
+        </div>
+
+        <div className="row-item">
+          <div className="col-item col-date">
+            Thứ 4
+          </div>
+          <div className="col-item col-time">
+            17:00 - 19:00
+          </div>
+          <div className="col-item col-action">
+            <Button
+              variant="outlined"
+              className="my-btn btn-blue btn-outlined">
+              Đặt lịch hẹn
+            </Button>
+          </div>
+        </div>
+
+        <div className="row-item">
+          <div className="col-item col-date">
+            Thứ 5
+          </div>
+          <div className="col-item col-time">
+            17:00 - 19:00
+          </div>
+          <div className="col-item col-action">
+            <Button
+              variant="outlined"
+              className="my-btn btn-blue btn-outlined">
+              Đặt lịch hẹn
+            </Button>
+          </div>
+        </div>
+
+        <div className="row-item">
+          <div className="col-item col-date">
+            Thứ 6
+          </div>
+          <div className="col-item col-time">
+            17:00 - 19:00
+          </div>
+          <div className="col-item col-action">
+            <Button
+              variant="outlined"
+              className="my-btn btn-blue btn-outlined">
+              Đặt lịch hẹn
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
   return (
     <div className="wrapper-hospital-detail-page">
-      <div className="left-content">
-        <Tabs
-          className="my-vertical-tab"
-          orientation="vertical"
-          value={value}
-          onChange={handleChange}
-          aria-label="Vertical tabs hospital">
-          <Tab label="Giới thiệu bệnh viện" {...a11yProps(0)} />
-          <Tab label="Khoa nội thận - tiết niệu" {...a11yProps(1)} />
-          <Tab label="Khoa tiêu hóa" {...a11yProps(2)} />
-          <Tab label="Khoa cơ xương khớp" {...a11yProps(3)} />
-          <Tab label="Khoa chấn thương chỉnh hình" {...a11yProps(4)} />
-        </Tabs>
-      </div>
-      <div className="right-content">
-        <TabPanel value={value} index={0}>
-          Giới thiệu bệnh viện
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Khoa nội thận - tiết niệu
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          Khoa tiêu hóa
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-          Khoa cơ xương khớp
-        </TabPanel>
-        <TabPanel value={value} index={4}>
-          Khoa chấn thương chỉnh hình
-        </TabPanel>
+      <div className="container-app">
+        <div className="content-hospital">
+          <div className="left-content">
+            <Tabs
+              className="my-vertical-tab"
+              orientation="vertical"
+              value={value}
+              onChange={handleChange}
+              aria-label="Vertical tabs hospital">
+              <Tab label="Giới thiệu bệnh viện" {...a11yProps(0)} />
+              <Tab label="Khoa nội thận - tiết niệu" {...a11yProps(1)} />
+              <Tab label="Khoa tiêu hóa" {...a11yProps(2)} />
+              <Tab label="Khoa cơ xương khớp" {...a11yProps(3)} />
+              <Tab label="Khoa chấn thương chỉnh hình" {...a11yProps(4)} />
+            </Tabs>
+          </div>
+          <div className="right-content">
+            <TabPanel value={value} index={0}>
+              <div className="wrapper-right-content">
+                  {GeneralInfo()}
+                  {DetailInfo()}
+                  {TimeWork()}
+              </div>
+
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              Khoa nội thận - tiết niệu
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              Khoa tiêu hóa
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+              Khoa cơ xương khớp
+            </TabPanel>
+            <TabPanel value={value} index={4}>
+              Khoa chấn thương chỉnh hình
+            </TabPanel>
+          </div>
+        </div>
       </div>
     </div>
   );
